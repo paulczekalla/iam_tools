95cf3577989c010f9993da6ed8e4320cdbd07a32
paulczekalla <paul.czekalla+dev@gmail.com>
2013-11-03 23:25:58 +0100

    Started new Feature 'Generate Default Referer URL by Site'; Added abstract Worker

---
+++ defaultUrlGenerator.py
@@ -0,0 +1,40 @@
+﻿import json
+import requests
+from lib.auth import Auth,AuthException
+
+from lib.httpHandler import HttpHandler
+from worker.generic.AbstractGenericWorker import AbstractGenericWorker
+
+def aquireAuthToken(authObj, http):
+	token = ''
+	try:
+		token = authObj.readResponse(authObj.authorizationRequest(http))
+	except AuthException as e:
+		print('Login mit Zugang {} nicht möglich.'.format(e.login))
+		print('Zugangsdaten erneut eingeben: ')
+		login = input('Login: ')
+		password = input('Passwort: ')
+		aquireAuthToken(Auth(login, password), http)
+	else:
+		print(token)
+		http.setToken(token)
+
+http = HttpHandler()
+
+a = Auth('Auth', 'here')
+aquireAuthToken(a, http)
+
+worker = AbstractGenericWorker(http)
+
+placements = worker.getAllEntitiesByType('placement')
+
+for place in placements:
+	print(str(place['id']) + ' ' + str(place['site_id']))
+	hex_id = str(hex(place['site_id'])).split("0x")[1]
+	print("www.interactivemedia-" + str(hex_id) + ".net")
+
+payload = {"placement":{"default_referrer_url":None}}
+
+params = {'id':placements[3]['id']}
+reqRet = http.putRequest(payload, 'placement', params)
+print(reqRet.json()['response']['status'])
---
+++ worker/generic/AbstractGenericWorker.py
@@ -0,0 +1,18 @@
+class AbstractGenericWorker:
+	def __init__(self, http):
+		self.http = http
+
+	def getAllEntitiesByType(self, type):
+		firstReturn = self.http.getRequestPage(0, type).json()['response']
+		count = firstReturn['count']
+
+		allEntities = list()
+		# very ugly hack with the add of the letter s
+		# for later versions maybe a dict with every plural version
+		allEntities.extend(firstReturn[type+'s'])
+
+		if count > 100:
+			for start_element in range(100, count, 100):
+				# again this plural s implementation 
+				allEntities.extend(self.http.getRequestPage(start_element, type).json()['response'][type+'s'])
+		return allEntities
