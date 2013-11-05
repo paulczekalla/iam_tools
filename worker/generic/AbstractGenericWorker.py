663c159780d50a44de519f67efcd71b984048112
Paul Czekalla <p.czekalla@interactivemedia.net>
2013-11-05 14:30:17 +0100

    Added Get Entities by Range feature

--- worker/generic/AbstractGenericWorker.py
+++ worker/generic/AbstractGenericWorker.py
@@ -17,3 +17,19 @@ class AbstractGenericWorker:
 				allEntities.extend(self.http.getRequestPage(start_element, type).json()['response'][type+'s'])
 		
 		return allEntities
+
+	def getAllEntitiesByRange(self, type, start_element, stop_element):
+		firstReturn = self.http.getRequestPage(start_element, type).json()['response']
+		count = firstReturn['count']
+
+		allEntities = list()
+		# very ugly hack with the add of the letter s
+		# for later versions maybe a dict with every plural version
+		allEntities.extend(firstReturn[type+'s'])
+		
+		if count > 100:
+			for start_element in range(start_element+100, stop_element, stop_element - start_element+100):
+				# again this plural s implementation 
+				allEntities.extend(self.http.getRequestPage(start_element, type).json()['response'][type+'s'])
+		
+		return allEntities
