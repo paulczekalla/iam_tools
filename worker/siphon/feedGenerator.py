bc1d50121eca12da3714afa7e5c96926a91c60f2
Paul Czekalla <p.czekalla@interactivemedia.net>
2013-11-12 17:44:46 +0100

    Functionality for downloading only set feed types (+ only segment feed active)

--- worker/siphon/feedGenerator.py
+++ worker/siphon/feedGenerator.py
@@ -6,7 +6,8 @@ class FeedGenerator:
 		self.http = http
 		self.service = 'siphon'
 
-		self.feedTypes = ("standard_feed", "bid_landscape_feed","segment_feed", "custom_data_feed")
+		#self.feedTypes = ("standard_feed", "bid_landscape_feed","segment_feed", "custom_data_feed")
+		self.feedTypes = ("segment_feed")
 		self.ready_for_download = ("new", "pending", "in_progress", "completed")
 
 	def getFeeds(self):
@@ -21,10 +22,11 @@ class FeedGenerator:
 		siphon_downloadlinks = list()
 		for feed in datafeeds:
 			for split_part in feed["splits"]:
-				params = {"siphon_name":feed["name"],"hour":feed["hour"], "timestamp":feed["timestamp"], "split_part":split_part["part"]}
-				if split_part["status"] in self.ready_for_download:
-					siphon_downloadlinks.append(params)
-				#download_link = self.http.getRequest("siphon-download", params)
-				#print(download_link.headers)
+				if feed["name"] in self.getFeedTypes():
+					params = {"siphon_name":feed["name"],"hour":feed["hour"], "timestamp":feed["timestamp"], "split_part":split_part["part"]}
+					if split_part["status"] in self.ready_for_download:
+						siphon_downloadlinks.append(params)
+					#download_link = self.http.getRequest("siphon-download", params)
+					#print(download_link.headers)
 
 		return siphon_downloadlinks
